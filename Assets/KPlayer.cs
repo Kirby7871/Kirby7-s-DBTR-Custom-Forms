408e9f4acf382d4896ad050838696bffcb49f163
Kirby7871 <123486466+Kirby7871@users.noreply.github.com>
2023-07-13 14:44:04 +0200

    Add files via upload

--- Assets/KPlayer.cs
+++ Assets/KPlayer.cs
@@ -67,7 +67,38 @@ namespace K7DBTRF.Assets
 
         public int LSSJ10FPTimer;
 
-        
+        public bool SSJ5Active; //This is a field
+        public bool SSJ5UnlockMsg;
+        public bool SSJ5Achieved;
+
+        public bool SSJ6Active; //This is a field
+        public bool SSJ6UnlockMsg;
+        public bool SSJ6Achieved;
+
+        public bool SSJ7Active;
+        public bool SSJ7UnlockMsg;
+        public bool SSJ7Achieved(Player player)
+        {
+            return GPlayer.ModPlayer(player).GetMastery(ModContent.BuffType<SSJ6Buff>()) >= 1f;
+        } //This is a method
+
+        public bool SSJ8Active; //This is a field
+        public bool SSJ8UnlockMsg;
+        public bool SSJ8Achieved;
+
+        public bool SSJ9Active; //This is a field
+        public bool SSJ9UnlockMsg;
+        public bool SSJ9Achieved;
+
+        public bool SSJ9FPActive;
+        public bool SSJ9FPUnlockMsg;
+        public bool SSJ9FPAchieved;
+
+        public bool SSJ10FPActive; //This is a field
+        public bool SSJ10FPUnlockMsg;
+        public bool SSJ10FPAchieved;
+
+        public int SSJ10FPTimer;
 
         private TransformationInfo? Form = null;
 
@@ -139,6 +170,10 @@ namespace K7DBTRF.Assets
 
         public static bool MasteredLSSJ8(Player player) => GPlayer.ModPlayer(player).GetMastery(ModContent.BuffType<Buffs.LSSJ8Buff>()) >= 1f;
 
+
+
+        public static bool MasteredSSJ6(Player player) => GPlayer.ModPlayer(player).GetMastery(ModContent.BuffType<Buffs.SSJ6Buff>()) >= 1f;
+
         public override void SaveData(TagCompound tag)
         {
             var vplayer = Main.CurrentPlayer;
@@ -146,7 +181,6 @@ namespace K7DBTRF.Assets
             tag.Add("K7DBTRF_LSSJ5UnlockMsg", LSSJ5UnlockMsg);
             tag.Add("K7DBTRF_LSSJ6Achieved", LSSJ6Achieved);
             tag.Add("K7DBTRF_LSSJ6UnlockMsg", LSSJ6UnlockMsg);
-            tag.Add("K7DBTRF_LSSJ7Achieved", LSSJ7Achieved(vplayer));
             tag.Add("K7DBTRF_LSSJ7UnlockMsg", LSSJ7UnlockMsg);
             tag.Add("K7DBTRF_LSSJ8Achieved", LSSJ8Achieved);
             tag.Add("K7DBTRF_LSSJ8UnlockMsg", LSSJ8UnlockMsg);
@@ -159,6 +193,25 @@ namespace K7DBTRF.Assets
 
             tag.Add("K7DBTRF_LSSJ10FPAchieved", LSSJ10FPAchieved);
             tag.Add("K7DBTRF_LSSJ10FPUnlockMsg", LSSJ10FPUnlockMsg);
+
+
+
+            tag.Add("K7DBTRF_SSJ5Achieved", SSJ5Achieved);
+            tag.Add("K7DBTRF_SSJ5UnlockMsg", SSJ5UnlockMsg);
+
+            tag.Add("K7DBTRF_SSJ6Achieved", SSJ6Achieved);
+            tag.Add("K7DBTRF_SSJ6UnlockMsg", SSJ6UnlockMsg);
+
+            tag.Add("K7DBTRF_SSJ7UnlockMsg", SSJ7UnlockMsg);
+
+            tag.Add("K7DBTRF_SSJ8Achieved", SSJ8Achieved);
+            tag.Add("K7DBTRF_SSJ8UnlockMsg", SSJ8UnlockMsg);
+
+            tag.Add("K7DBTRF_SSJ9Achieved", SSJ9Achieved);
+            tag.Add("K7DBTRF_SSJ9UnlockMsg", SSJ9UnlockMsg);
+
+            tag.Add("K7DBTRF_SSJ10FPAchieved", SSJ10FPAchieved);
+            tag.Add("K7DBTRF_SSJ10FPUnlockMsg", SSJ10FPUnlockMsg);
         }
 
         public override void LoadData(TagCompound tag)
@@ -170,7 +223,7 @@ namespace K7DBTRF.Assets
 
             if (tag.ContainsKey("K7DBTRF_LSSJ6Achieved"))
                 LSSJ6Achieved = tag.GetBool("K7DBTRF_LSSJ6Achieved");
-            if (tag.ContainsKey("K7DBTRF_LSSJ5UnlockMsg"))
+            if (tag.ContainsKey("K7DBTRF_LSSJ6UnlockMsg"))
                 LSSJ6UnlockMsg = tag.GetBool("K7DBTRF_LSSJ6UnlockMsg");
 
             if (tag.ContainsKey("K7DBTRF_LSSJ8Achieved"))
@@ -192,6 +245,38 @@ namespace K7DBTRF.Assets
                 LSSJ10FPAchieved = tag.GetBool("K7DBTRF_LSSJ10FPAchieved");
             if (tag.ContainsKey("K7DBTRF_LSSJ10FPUnlockMsg"))
                 LSSJ10FPUnlockMsg = tag.GetBool("K7DBTRF_LSSJ10FPUnlockMsg");
+
+
+
+            if (tag.ContainsKey("K7DBTRF_SSJ5Achieved"))
+                SSJ5Achieved = tag.GetBool("K7DBTRF_SSJ5Achieved");
+            if (tag.ContainsKey("K7DBTRF_SSJ5UnlockMsg"))
+                SSJ5UnlockMsg = tag.GetBool("K7DBTRF_SSJ5UnlockMsg");
+
+            if (tag.ContainsKey("K7DBTRF_SSJ6Achieved"))
+                SSJ6Achieved = tag.GetBool("K7DBTRF_SSJ6Achieved");
+            if (tag.ContainsKey("K7DBTRF_LSSJ5UnlockMsg"))
+                SSJ6UnlockMsg = tag.GetBool("K7DBTRF_SSJ6UnlockMsg");
+
+            if (tag.ContainsKey("K7DBTRF_SSJ8Achieved"))
+                SSJ8Achieved = tag.GetBool("K7DBTRF_SSJ8Achieved");
+            if (tag.ContainsKey("K7DBTRF_SSJ8UnlockMsg"))
+                SSJ8UnlockMsg = tag.GetBool("K7DBTRF_SSJ8UnlockMsg");
+
+            if (tag.ContainsKey("K7DBTRF_SSJ9Achieved"))
+                SSJ9Achieved = tag.GetBool("K7DBTRF_SSJ9Achieved");
+            if (tag.ContainsKey("K7DBTRF_SSJ9UnlockMsg"))
+                SSJ9UnlockMsg = tag.GetBool("K7DBTRF_SSJ9UnlockMsg");
+
+            if (tag.ContainsKey("K7DBTRF_SSJ9FPAchieved"))
+                SSJ9FPAchieved = tag.GetBool("K7DBTRF_SSJ9FPAchieved");
+            if (tag.ContainsKey("K7DBTRF_LSSJ9FPUnlockMsg"))
+                SSJ9FPUnlockMsg = tag.GetBool("K7DBTRF_SSJ9FPUnlockMsg");
+
+            if (tag.ContainsKey("K7DBTRF_SSJ10FPAchieved"))
+                SSJ10FPAchieved = tag.GetBool("K7DBTRF_SSJ10FPAchieved");
+            if (tag.ContainsKey("K7DBTRF_SSJ10FPUnlockMsg"))
+                SSJ10FPUnlockMsg = tag.GetBool("K7DBTRF_SSJ10FPUnlockMsg");
         }
 
         public override void OnRespawn(Player player)
@@ -346,6 +431,126 @@ namespace K7DBTRF.Assets
         {
             var Kplayer = player.GetModPlayer<KPlayer>();
             return Kplayer.LSSJ9Achieved;
+        }
+    }
+
+
+
+    public class SSJAFPanel : TransformationTree
+    {
+        public override bool Complete() => true;
+
+        public override bool Condition(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            var Bplayer = player.GetModPlayer<BPlayer>();
+            return player.GetModPlayer<GPlayer>().Trait != "Legendary";
+        }
+
+        public override Connection[] Connections() => new Connection[]
+        {
+            new Connection(0,2,1,false,new Gradient(Color.White).AddStop(0.60f, new Color(0, 0, 255))),
+            new Connection(1,2,1,false,new Gradient(Color.White).AddStop(0.60f, new Color(0, 0, 255))),
+            new Connection(2,2,1,false,new Gradient(Color.Blue).AddStop(0.60f, new Color(0, 255, 0))),
+            new Connection(3,2,1,false,new Gradient(Color.Yellow).AddStop(0.60f, new Color(30, 30, 30))),
+            new Connection(4,2,1,false,new Gradient(Color.Yellow).AddStop(0.60f, new Color(30, 30, 30))),
+            new Connection(5,2,1,false,new Gradient(Color.Yellow).AddStop(0.60f, new Color(30, 30, 30)))
+        };
+
+        public override string Name() => "Alternative Future";
+
+        public override Node[] Nodes() => new Node[]
+        {
+            new Node(0, 2, "SSJ5Buff", "K7DBTRF/Buffs/SSJ5Buff", "You should kill the moon lord again to improve your new power",UnlockConditionSSJ5,DiscoverConditionSSJ5),
+            new Node(1, 2, "SSJ6Buff", "K7DBTRF/Buffs/SSJ6Buff", "Consume the 4 star dragon ball combined with evil prideful essence",UnlockConditionSSJ6,DiscoverConditionSSJ6),
+            new Node(2, 2, "SSJ7Buff", "K7DBTRF/Buffs/SSJ7Buff", "I think you should just train harder with your new form",UnlockConditionSSJ7,DiscoverConditionSSJ7),
+            new Node(3, 2, "SSJ8Buff", "K7DBTRF/Buffs/SSJ8Buff", "Can you defeat a crazed cultist after mastering many forms?",UnlockConditionSSJ8,DiscoverConditionSSJ8),
+            new Node(4, 2, "SSJ9Buff", "K7DBTRF/Buffs/SSJ9Buff", "The emblem of the guardian of hell could be infused wth pure ki and unlock new power", UnlockConditionSSJ9, DiscoverConditionSSJ9),
+            new Node(5, 2, "SSJ9FPBuff", "K7DBTRF/Buffs/SSJ9FPBuff", "See SSJ9", UnlockConditionSSJ9FP, DiscoverConditionSSJ9FP),
+            new Node(6, 2, "SSJ10FPBuff", "K7DBTRF/Buffs/SSJ10FPBuff", "Eat the voodoo doll of your first friend, and you will get dark power", UnlockConditionSSJ10FP, DiscoverConditionSSJ10FP)
+        };
+
+        public bool UnlockConditionSSJ5(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ5Achieved;
+        }
+
+        public bool DiscoverConditionSSJ5(Player player)
+        {
+            return true;
+        }
+
+        public bool UnlockConditionSSJ6(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ6Achieved;
+        }
+
+        public bool DiscoverConditionSSJ6(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ5Achieved;
+        }
+
+        public bool UnlockConditionSSJ7(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ7Achieved(player);
+        }
+
+        public bool DiscoverConditionSSJ7(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ6Achieved;
+        }
+
+        public bool UnlockConditionSSJ8(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ8Achieved;
+        }
+
+        public bool DiscoverConditionSSJ8(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ6Achieved;
+        }
+
+        public bool UnlockConditionSSJ9(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ9Achieved;
+        }
+
+        public bool DiscoverConditionSSJ9(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ8Achieved;
+        }
+
+        public bool UnlockConditionSSJ9FP(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ9FPAchieved;
+        }
+
+        public bool DiscoverConditionSSJ9FP(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ8Achieved;
+        }
+
+        public bool UnlockConditionSSJ10FP(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ10FPAchieved;
+        }
+
+        public bool DiscoverConditionSSJ10FP(Player player)
+        {
+            var Kplayer = player.GetModPlayer<KPlayer>();
+            return Kplayer.SSJ9Achieved || Kplayer.SSJ9FPAchieved;
         }
     }
 }
