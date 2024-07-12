71f36838c19d11bfb9d1196d6a19d25642a023ce
Kirby7871 <123486466+Kirby7871@users.noreply.github.com>
2024-07-12 05:14:43 +0200

    Add files via upload

---
+++ Assets/Assets/BalanceConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace K7DBTRF.Assets
+{
+    //[Label("Client Settings")]
+    internal class BalanceConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        public static BalanceConfig Instance;
+
+        //[Header("Legendary Saiyan 5 Settings")]
+        [DefaultValue(false)]
+        public bool UseHair;
+    }
+    //[Label("Server Settings")]
+    internal class BalanceConfigServer : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        public static BalanceConfigServer Instance;
+
+        //[Header("Toggleable Balance Adjustments")]
+        [DefaultValue(true)]
+        public bool ChargeRework;
+
+        
+
+        
+        [DefaultValue(true)]
+        public bool LongerTransform;
+
+        
+        [DefaultValue(true)]
+        public bool SSJTweaks;
+    }
+}
